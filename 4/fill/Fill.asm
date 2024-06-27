e6e388b8d465dfbb5d1ad783bc3ad7de1f2d1ba7
Lloyd <lloydgyt@outlook.com>
2024-06-27 19:43:20 +0800

    [feat] complete Fill.asm

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -9,3 +9,70 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+
+// initialize parameters
+@8192
+D=A
+@words
+M=D // words = 8192;
+
+(LOOP)
+// probe the KBD
+	@KBD
+	D=M
+
+	// initialize i = 0
+	@i
+	M=0 
+
+	// if KBD != 0; draw black;
+	@DRAWBLACK
+	D;JNE
+
+	// else if KBD == 0; draw white
+	(DRAWWHITE)
+		@i
+		D=M
+		@SCREEN
+		A=A+D
+		M=0 // RAM[SCREEN + i] = 0
+				 // this effectively set the word white
+
+		@i
+		M=M+1 // i++
+
+		// if i < words, continue
+		@i
+		D=M
+		@words
+		D=D-M
+		@DRAWWHITE
+		D;JLT
+
+	// continue this loop
+	@LOOP
+	D;JEQ
+
+
+	(DRAWBLACK)
+		@i
+		D=M
+		@SCREEN
+		A=A+D
+		M=-1 // RAM[SCREEN + i] = -1
+				 // this effectively set the word black
+
+		@i
+		M=M+1 // i++
+
+		// if i < words, continue
+		@i
+		D=M
+		@words
+		D=D-M
+		@DRAWBLACK
+		D;JLT
+
+	@LOOP
+	0;JMP // keep looping
+
